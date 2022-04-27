418e12385eb5e4b2bb241c83ba33b0406d83020c
Aung Kyaw Nyunt <aungkyawnyunt2004@gmail.com>
2022-04-28 05:55:34 +0630

    [create] validator

---
+++ application/functions/validator.function.js
@@ -0,0 +1,92 @@
+
+class Validator{
+
+    #promiseList=[];
+    #_input;
+    #_key;
+    #_inputErr;
+    constructor(){
+        this.errors = {};
+    }
+
+    static Rule= async (_callBack) =>{
+        const validator = new Validation();
+        await  _callBack(validator);
+        return validator;
+        
+    }
+
+    validate = ()=>{
+       return Promise.all(this.#promiseList).then((values) => {s
+            return Object.keys(this.errors).length==0
+        });
+    }
+
+    input=(_key,_input)=>{
+        this.#_input = _input;
+        this.#_key = _key;
+        this.#_inputErr = [];
+        return this;
+    }
+
+    isEmpty= (_msg)=>{
+        let me = this;
+        let obj = new Promise(resolve=>{
+            if(this.#_input === undefined){
+                this.#_inputErr.push(_msg);
+                this.errors[this.#_key] = this.#_inputErr;
+            }
+            else if(this.#_input==""){  
+                this.#_inputErr.push(_msg);
+                this.errors[this.#_key] = this.#_inputErr;
+            }
+            resolve(me);
+        });
+
+        this.#promiseList.push(obj);
+        return this;
+    }
+    isDate=async (_msg)=>{
+        const isValidDate=(_d)=>{
+            let d = new Date(_d);
+            return d instanceof Date && !isNaN(d);
+        }
+        let me = this;
+        let obj = new Promise(resolve=>{
+            if(this.#_input === undefined){
+                this.#_inputErr.push(_msg);
+                this.errors[this.#_key] = this.#_inputErr;
+            }
+            else if(!isValidDate(this.#_input)){  
+                this.#_inputErr.push(_msg);
+                this.errors[this.#_key] = this.#_inputErr;
+            }
+            resolve(me);
+        });
+
+        this.#promiseList.push(obj);
+        return this;
+    }
+
+    customFunction=async (_callback)=>{
+        //await _callback(this);
+
+        //let me = this;
+        let me = this;
+        let obj = new Promise(async resolve=>{
+            await _callback(me);
+            resolve(me);
+            
+        })
+        this.#promiseList.push(obj);
+        return this;
+    }
+    setError=(_msg)=>{
+        this.#_inputErr.push(_msg);
+        this.errors[this.#_key] = this.#_inputErr;
+    }
+
+}
+
+
+module.exports  = Validator;
--- application/services/businessday/calculate.js
+++ application/services/businessday/calculate.js
@@ -1,6 +1,15 @@
 const {StatusCodes} = require('API/config');
-// program is aws lambda function style
+const Validation = require('API/functions/validation');
+// program base on aws lambda function style
 exports.handler = async (event,callback) => {
+
+    const formData = event.body;
+    console.log('formData',formData);
+    const v = await Validation.Rule(
+        async validator=>{
+            validator.input("brand_name",formData.brand_name)
+            .isEmpty("Brand Name should not be empty");
+        });
     return {
         statusCode: StatusCodes.Ok,
         body: {"hello":"world"}
